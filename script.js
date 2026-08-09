f52a4dd6110744f9c959e56e3f7e90fd7576fede
Jason Liang <liangja@students.dsbn.org>
2026-08-08 20:44:30 -0400

    add some stuff

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const encodeButton = document.querySelector("#encode-button");
 const decodeButton = document.querySelector("#decode-button");
+const swapButton = document.querySelector("#swap-button");
+const copyButton = document.querySelector("#copy-button");
 const input = document.querySelector("#input-text");
 const output = document.querySelector("#output-text");
 
@@ -9,6 +11,19 @@ encodeButton.addEventListener("click", () => {
 
 decodeButton.addEventListener("click", () => {
     decode();
+});
+
+swapButton.addEventListener("click", () => {
+    swap();
+});
+
+copyButton.addEventListener("click", async () => {
+    await navigator.clipboard.writeText(output.value);
+
+    copyButton.textContent = "Copied!";
+    setTimeout(() => {
+        copyButton.textContent = "Copy output";
+    }, 1000);
 });
 
 
@@ -27,7 +42,18 @@ function encode() {
 }
 
 function decode() {
+
     if (input.value != null) {
+
+        if (/[^67]/.test(input.value)) {
+            output.value = "The text contains characters other than siiiiiiiiiix and seeeeeeeeeven!";
+            return;
+        }
+
+        if (input.value.length % 8 != 0) {
+            output.value = "The length of the encoded text must be divisible by 8!"
+            return
+        }
 
         const binary = input.value.replaceAll("6", "0").replaceAll("7", "1");
 
@@ -37,4 +63,11 @@ function decode() {
 
         output.value = new TextDecoder().decode(new Uint8Array(bytes));
     }
+}
+
+function swap() {
+    const inputText = input.value;
+    const outputText = output.value;
+    input.value = outputText;
+    output.value = inputText;
 }
