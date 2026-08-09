d25d805687212eefe42076718f18528c5251961a
Jason Liang <liangja@students.dsbn.org>
2026-08-08 20:13:04 -0400

    add readme

--- script.js
+++ script.js
@@ -4,35 +4,37 @@ const input = document.querySelector("#input-text");
 const output = document.querySelector("#output-text");
 
 encodeButton.addEventListener("click", () => {
+    encode();
+})
+
+decodeButton.addEventListener("click", () => {
+    decode();
+});
+
+
+function encode() {
     if (input.value != null) {
 
         const bytes = new TextEncoder().encode(input.value);
 
         output.value = Array.from(bytes)
-            .map(bytes => bytes.toString(2).padStart(8, "0"))
+            .map(byte => byte.toString(2).padStart(8, "0"))
             .join("")
             .replaceAll("0", "6")
             .replaceAll("1", "7");
 
-
     }
-})
+}
 
-decodeButton.addEventListener("click", () => {
-    const encoded = input.value;
+function decode() {
+    if (input.value != null) {
 
-    if (encoded) {
-        // 6 -> 0, 7 -> 1
-        const binary = encoded
-            .replaceAll("6", "0")
-            .replaceAll("7", "1");
+        const binary = input.value.replaceAll("6", "0").replaceAll("7", "1");
 
-        // Split into 8-bit chunks and convert to bytes
-        const bytes = binary.match(/.{8}/g).map(byte =>
-            parseInt(byte, 2)
-        );
+        const bytes = binary
+            .match(/.{8}/g)
+            .map(byte => parseInt(byte, 2));
 
-        // UTF-8 bytes -> string
         output.value = new TextDecoder().decode(new Uint8Array(bytes));
     }
-});
+}
